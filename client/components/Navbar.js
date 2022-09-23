b40e1c9f6adf4ee0403e24567024718ed377a60d
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-09-23 18:10:36 -0400

    Starting navbar config

---
+++ client/components/Navbar.js
@@ -0,0 +1,15 @@
+
+
+export default function Navbar() {
+
+    return (
+    <>
+        <div className="mx-auto py-8 sm:px-4 lg:px-8 bg-red-600 shadow flex-row justify-around">
+            Nigga
+        </div>
+        <div className="mx-auto py-2 sm:px-4 lg:px-8 bg-emerald-400 shadow">
+            Nuggs
+        </div>
+    </>
+    )
+}
