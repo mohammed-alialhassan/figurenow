a54378c95d51ef438faf2676cc0cac00bd28d4ce
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-09-27 19:53:20 -0400

    Renamed new file to home.js, starting work on step by step form

---
+++ client/pages/calorie_counter.js
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function CalorieCounter() {
+
+    return (
+        <>
+            <main>
+                <Navbar />
+                <div className="relative min-h-screen pb-40  bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
+
+                </div>
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </>
+    )
+}
