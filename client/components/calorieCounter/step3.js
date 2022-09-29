e356c5cb694772a42738debe04c21a7db17f41c2
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-09-29 17:52:07 -0400

    Finished layout of step 3

--- client/components/calorieCounter/step3.js
+++ client/components/calorieCounter/step3.js
@@ -13,7 +13,7 @@ export default function Step3() {
             <div className="flex-col w-5/12 lg:w-5/12 justify-center">
               <label
                 htmlFor="weight"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-lg font-medium text-gray-700"
               >
                 Weight
               </label>
@@ -22,8 +22,8 @@ export default function Step3() {
                   type="weight"
                   name="weight"
                   id="weight"
-                  className="block w-full rounded-md border-red-300 pr-10 text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                  placeholder="In lbs please!"
+                  className="block w-full py-1 lg:py-3 rounded-md border-red-300 pr-10 text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
+                  placeholder="Pounds (lbs)"
                   aria-invalid="true"
                   aria-describedby="weight-error"
                 />
@@ -39,25 +39,25 @@ export default function Step3() {
               </p>
             </div>
 
-            <div className="flex-row w-6/12 justify-center">
+            <div className="flex-row w-6/12 lg:w-5/12 justify-center">
               <label
                 htmlFor="height"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-lg font-medium text-gray-700"
               >
                 Height
               </label>
-              <div className="flex gap-x-2 justify-center">
+              <div className="flex gap-x-2 lg:w-10/12 justify-between">
                 <div className="relative mt-1  rounded-md shadow-sm">
                 <input
-                  type="height"
-                  name="height"
-                  id="height"
-                  className="block w-full rounded-md border-red-300 pr-10 text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                  placeholder="Never find"
+                  type="ft"
+                  name="ft"
+                  id="ft"
+                  className="block w-full py-1 lg:py-3  rounded-md border-red-300  text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
+                  placeholder="Feet (ft)"
                   aria-invalid="true"
-                  aria-describedby="height-error"
+                  aria-describedby="ft-error"
                 />
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-1">
                   <ExclamationCircleIcon
                     className="h-5 w-5 text-red-500"
                     aria-hidden="true"
@@ -67,15 +67,15 @@ export default function Step3() {
 
               <div className="relative mt-1 rounded-md shadow-sm">
                 <input
-                  type="height"
-                  name="height"
-                  id="height"
-                  className="block w-full rounded-md border-red-300 pr-10 text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
-                  placeholder="Big botty fish"
+                  type="inch"
+                  name="inch"
+                  id="inch"
+                  className="block w-full py-1 lg:py-3  rounded-md border-red-300  text-black placeholder-gray-300 focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm"
+                  placeholder="Inches (in)"
                   aria-invalid="true"
-                  aria-describedby="height-error"
+                  aria-describedby="inch-error"
                 />
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-1">
                   <ExclamationCircleIcon
                     className="h-5 w-5 text-red-500"
                     aria-hidden="true"
@@ -85,7 +85,7 @@ export default function Step3() {
               </div>
               
               <p className="mt-2 text-sm text-red-600" id="weight-error">
-                Your password must be less than 4 characters.
+                You must enter a valid height.
               </p>
             </div>
           </div>
