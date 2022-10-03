692ef58fff24b386148db1e1853f0fa3f6490136
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-10-03 16:10:39 -0400

    Added progress bar to track questionaire progress

--- client/components/calorieCounter/step1.js
+++ client/components/calorieCounter/step1.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 
-
-export default function Step1({setGender}) {
-  
-// { maleGender, femaleGender }
+export default function Step1({ setGender }) {
+  // { maleGender, femaleGender }
   /* const handleClick1 = () => {
 
     let male = {"gender": "Male"};
@@ -13,38 +11,46 @@ export default function Step1({setGender}) {
     }))
 
   } */
-  
+
   const male = "Male";
   const female = "Female";
 
-    return (
-        <>
-            <div className="flex justify-center lg:py-2 text-3xl text-center lg:text-4xl text-white">
-            <h1> What gender were you born as? </h1>
+  return (
+    <>
+      <div className="flex justify-center py-2 lg:py-4 text-3xl text-center lg:text-4xl text-white">
+        <h1> What gender were you born as? </h1>
+      </div>
+      <div className="w-full bg-gray-200">
+        <div
+          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-r-full"
+          style={{ width: "4%" }}
+        >
+          {" "}
+          0%
+        </div>
+      </div>
+      <div className="flex-col justify-center relative h-1/2 w-full py-16 lg:py-40 bg-gray-100 shadow-lg outline outline-1">
+        <div className="flex justify-center">
+          <div className="flex justify-between gap-x-4 w-9/12 2xl:w-8/12 py-4">
+            <button
+              type="button"
+              onClick={() => setGender(male)}
+              className="inline-flex items-center rounded border border-transparent bg-green-200 px-12 py-3 lg:px-24 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2"
+            >
+              Male
+            </button>
+            <button
+              type="button"
+              className="inline-flex items-center rounded border border-transparent bg-green-200 px-12 py-3 lg:px-24 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2"
+              onClick={() => setGender(female)}
+            >
+              Female
+            </button>
           </div>
-          <div className="flex-col justify-center relative h-1/2 w-full py-16 lg:py-40 bg-gray-100 shadow-lg outline outline-1">
-            <div className="flex justify-center">
-              <div className="flex justify-between gap-x-4 w-9/12 2xl:w-8/12 py-4">
-                <button
-                  type="button"
-                  onClick={() => setGender(male)}
-                  className="inline-flex items-center rounded border border-transparent bg-green-200 px-12 py-3 lg:px-24 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2"
-                  
-                >
-                  Male
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex items-center rounded border border-transparent bg-green-200 px-12 py-3 lg:px-24 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2"
-                  onClick={() => setGender(female)}
-                >
-                  Female
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
-    )
+        </div>
+      </div>
+    </>
+  );
 }
 //onClick={handleClick1()}
 //onClick={handleClick2()}
--- client/components/calorieCounter/step5.js
+++ client/components/calorieCounter/step5.js
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
 
-
 export default function Step5({ settingGoal }) {
+  let loseWeight = 0.7;
+  let buildMuscle = 1.1;
+  let transform = 0.9;
 
-    let loseWeight = 0.7;
-    let buildMuscle = 1.1;
-    let transform = 0.9;
-
-    return (
-        <>
-            <div className="flex justify-center lg:py-2 text-3xl text-center lg:text-4xl text-white">
-            <h1> What is your main goal? </h1>
-          </div>
-          
-          <div className="flex-col justify-center relative h-1/2 w-full py-16 lg:py-36 bg-gray-100 shadow-lg outline outline-1">
-            <div className="flex justify-between gap-x-4 lg:gap-x-2 w-12/12 2xl:w-10/12 py-2 px-5  lg:px-32 lg:pr-36 2xl:pr-2">
-                    <h1 className="text-lg lg:text-3xl"> Lose weight </h1>
-                    <h1 className="text-lg lg:text-3xl"> Build muscle </h1>
-                    <h1 className="text-lg lg:text-3xl"> Transform! </h1>
-                </div>
-            <div className="flex justify-center">
-              <div className="flex justify-between gap-x-4 w-11/12 2xl:w-10/12 py-4">
-              <div className="flex-col justify-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-sm lg:text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
-                <img src="https://images.unsplash.com/photo-1638183395699-2c0db5b6afbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="w-full h-16 lg:h-32 lg:w-96" onClick={() => settingGoal(loseWeight)}  />
-                </div>
-                <div className="inline-flex items-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
-                <img src="https://images.unsplash.com/photo-1532384661798-58b53a4fbe37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="w-full h-16 lg:h-32 lg:w-96" onClick={() => settingGoal(buildMuscle)} />
-                </div>
-                <div className="inline-flex items-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
-                <img src="https://images.unsplash.com/photo-1575052814074-c05122e0a17a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="w-full h-16 lg:h-32 lg:w-96" onClick={() => settingGoal(transform)} />
-                </div>
-              </div>
+  return (
+    <>
+      <div className="flex justify-center py-2 lg:py-4 text-3xl text-center lg:text-4xl text-white">
+        <h1> What is your main goal? </h1>
+      </div>
+      <div className="w-full bg-gray-100">
+        <div
+          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-r-full"
+          style={{ width: "80%" }}
+        >
+          {" "}
+          80%
+        </div>
+      </div>
+      <div className="flex-col justify-center relative h-1/2 w-full py-16 lg:py-36 bg-gray-100 shadow-lg outline outline-1">
+        <div className="flex justify-between gap-x-4 lg:gap-x-2 w-12/12 2xl:w-10/12 py-2 px-5  lg:px-32 lg:pr-36 2xl:pr-2">
+          <h1 className="text-lg lg:text-3xl"> Lose weight </h1>
+          <h1 className="text-lg lg:text-3xl"> Build muscle </h1>
+          <h1 className="text-lg lg:text-3xl"> Transform! </h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="flex justify-between gap-x-4 w-11/12 2xl:w-10/12 py-4">
+            <div className="flex-col justify-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-sm lg:text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
+              <img
+                src="https://images.unsplash.com/photo-1638183395699-2c0db5b6afbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                className="w-full h-16 lg:h-32 lg:w-96"
+                onClick={() => settingGoal(loseWeight)}
+              />
+            </div>
+            <div className="inline-flex items-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
+              <img
+                src="https://images.unsplash.com/photo-1532384661798-58b53a4fbe37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                className="w-full h-16 lg:h-32 lg:w-96"
+                onClick={() => settingGoal(buildMuscle)}
+              />
+            </div>
+            <div className="inline-flex items-center rounded border border-transparent bg-green-200 px-3 py-4 lg:px-12 lg:py-4 text-base font-medium text-green-700 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-2">
+              <img
+                src="https://images.unsplash.com/photo-1575052814074-c05122e0a17a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                className="w-full h-16 lg:h-32 lg:w-96"
+                onClick={() => settingGoal(transform)}
+              />
             </div>
           </div>
-        </>
-    )
+        </div>
+      </div>
+    </>
+  );
 }
