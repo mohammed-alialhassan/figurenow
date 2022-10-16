8b02294711a5dad9a07b645b6a1cd5af5d86685c
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-10-15 23:04:38 -0400

    About page completed

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -28,24 +28,9 @@ export default function Navbar() {
       <div className="hidden sm:block mx-auto py-2 sm:px-4 lg:px-32 lg:py xl:py bg-amber-400 shadow flex-row justify-around">
         <div className="flex justify-between">
           <Link href="/home">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-16 h-16 stroke-green-700 hover:stroke-white pt-1"
-
-          >
-            
-              <a>
-              <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M16.712 4.33a9.027 9.027 0 011.652 1.306c.51.51.944 1.064 1.306 1.652M16.712 4.33l-3.448 4.138m3.448-4.138a9.014 9.014 0 00-9.424 0M19.67 7.288l-4.138 3.448m4.138-3.448a9.014 9.014 0 010 9.424m-4.138-5.976a3.736 3.736 0 00-.88-1.388 3.737 3.737 0 00-1.388-.88m2.268 2.268a3.765 3.765 0 010 2.528m-2.268-4.796a3.765 3.765 0 00-2.528 0m4.796 4.796c-.181.506-.475.982-.88 1.388a3.736 3.736 0 01-1.388.88m2.268-2.268l4.138 3.448m0 0a9.027 9.027 0 01-1.306 1.652c-.51.51-1.064.944-1.652 1.306m0 0l-3.448-4.138m3.448 4.138a9.014 9.014 0 01-9.424 0m5.976-4.138a3.765 3.765 0 01-2.528 0m0 0a3.736 3.736 0 01-1.388-.88 3.737 3.737 0 01-.88-1.388m2.268 2.268L7.288 19.67m0 0a9.024 9.024 0 01-1.652-1.306 9.027 9.027 0 01-1.306-1.652m0 0l4.138-3.448M4.33 16.712a9.014 9.014 0 010-9.424m4.138 5.976a3.765 3.765 0 010-2.528m0 0c.181-.506.475-.982.88-1.388a3.736 3.736 0 011.388-.88m-2.268 2.268L4.33 7.288m6.406 1.18L7.288 4.33m0 0a9.024 9.024 0 00-1.652 1.306A9.025 9.025 0 004.33 7.288"
-            />
-            </a>
-          </svg>  
+            <a>
+              <img src="https://i.imgur.com/0tM7Fb7.png" className="pt-1 h-16 w-16" />
+            </a> 
           </Link>
           
           <div className="py-6 flex justify-between">
@@ -58,10 +43,11 @@ export default function Navbar() {
               Get Started!
             </a>
             </Link>
+            
           </div>
         </div>
       </div>
-      <div className="mx-auto py-2 sm:py-3 sm:px-4 lg:px-2 bg-green-600 shadow">
+      <div className="mx-auto py-2 sm:py-3 sm:px-4 lg:px-2 bg-green-600 lg:bg-green-700 shadow">
         <div className="sm:hidden block px-6 flex justify-between">
           <svg
             xmlns="http://www.w3.org/2000/svg"
